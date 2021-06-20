85993885817c2f915f624f1480e359a55c7bfbed
ampaire <femayapr@gmail.com>
2021-06-20 22:49:53 +0300

    Working with Strings

---
+++ convert_string.py
@@ -0,0 +1,14 @@
+import re
+
+def convert_string(sentence):
+  pattern = r'\d+'
+  new_sentence = re.sub(pattern, repl="", string=sentence)
+  
+  return new_sentence
+
+my_string1 = "The Quick Blue Bird1234 Flew Over the Fast River"
+my_string2 = "The Quick Blue Bird76521212 Flew Over the Fast River"
+
+
+print (convert_string(my_string1))
+print (convert_string(my_string2))
