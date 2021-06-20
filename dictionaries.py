ca2319f390df78f7c7438da3d0d42fd945611071
ampaire <femayapr@gmail.com>
2021-06-20 22:51:51 +0300

    Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+classroom = {
+  "person1":{"name":"Effie", "age":10, "school":"st Hellens"},
+  "person2":{"name":"Mark", "age": 11, "school":"Merryland"},
+  "person3":{"name":"Rachel", "age":13, "school":"Sir Apollo"},
+  "person4":{"name":"Collins", "age": 16, "school":"St. Joseph's"}
+}
+
+for i in classroom:
+  print(i, ":", classroom[i])
