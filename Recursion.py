d27b847849adc5b33540ffcf4428f8f5f44f7190
ampaire <femayapr@gmail.com>
2021-06-20 22:56:44 +0300

    Recursion

---
+++ Recursion.py
@@ -0,0 +1,10 @@
+def reAdd(a, m):
+  if a == 0: 
+    return m
+  elif a== m:
+    return a
+  else:
+    return a + reAdd(a + 1, m) 
+  
+
+print(reAdd(2,5)) #reAdd(2, 5) → 2 + reAdd(3, 5) → 2 + (3 + reAdd(4, 5)) → 2 + (3 + (4 +reAdd(5,5))) → 2+(3+(4+(5))) =14
